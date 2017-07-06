7279c86fecc2d9f4a68fec783f9f86700f1a6c05
Hifumisama <m.khayat92@gmail.com>
2017-07-06 23:36:47 +0200

    Besoin de tester le système de requêtes, donc le chapitre angular sur l objet http sera tres utile

--- EssecApp/src/pages/home/home.ts
+++ EssecApp/src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PeopleServiceProvider } from '../../providers/people-service/people-service';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers:[ PeopleServiceProvider ]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public peopleservice: PeopleServiceProvider
+  ) {
+    this.peopleservice.getResultRequest();
   }
 
 }
